570e1363ef2d4f9881f513cff65a28097bfd2c1f
AlicjaLisowska <61700083+AlicjaLisowska@users.noreply.github.com>
2020-03-02 16:36:05 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-unsigned int ns[] = { 10, /* TODO: fill in "n" i.e. instance sizes */ };
+unsigned int ns[] = {0,1,2,3,4,5,6,7,8,9 };
 
 void fill_increasing(int *t, unsigned int n) {
     // TODO: implement
